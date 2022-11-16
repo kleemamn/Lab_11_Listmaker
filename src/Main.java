dfca5d118e642ed0b35af7ff8a24a8b86afeddd2
kleemamn <kleemamn@mail.uc.edu>
2022-11-16 13:50:03 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> myArrList = new ArrayList<>();
+        final String menu = "A - Add D - Delete P - print Q - Quit";
+        boolean done = false;
+
+
+        do
+        {
+
+
+        }
+
+        while (!done);
+
+
+
+    }
+}
